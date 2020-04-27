aec27c092a383351c29fd3f68ad4197641ba07e1
Yogen Parekh <yogenparekh569@gmail.com>
2020-04-27 19:20:08 +0100

    pyramids

---
+++ pyramids.py
@@ -0,0 +1,9 @@
+# Printing Pyramids
+
+print('-----Full Pyramid-----')
+for i in range(5):
+	print(' '*(5-i-1) + '*'*(2*i+1))
+
+print('-----Reversed Pyramid-----')
+for i in reversed(range(5)):
+	print(' '*(5-i-1) + '*'*(2*i+1))
